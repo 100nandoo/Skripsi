e2f758b07d97a5f464f807daf7587922c1dd4a84
Fernando <100nandoo@gmail.com>
2016-10-09 22:44:47 +0700

    Tambah contoh tabel dan informasi teknis
    Terdapat penambahan di data.php. Serta penambahan di informasi_teknis

--- pages/data.php
+++ pages/data.php
@@ -1,4 +1,22 @@
-<div class="jumbotron text-center">
-	<h1>Data Page</h1>
-  	<p>{{ message }}</p>
+<div class="col-sm-4">
+	<table class="table table-striped table-bordered table-hover">
+		<thead>
+			<th>id</th>
+			<th>nama</th>
+			<th>uid</th>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Nando</td>
+				<td>Ha</td>
+				<td>haha@example.com</td>
+			</tr>
+			<tr>
+				<td>hehe</td>
+				<td>Hu</td>
+				<td>hege@example.com</td>
+			</tr>
+
+		</tbody>
+	</table>
 </div>
