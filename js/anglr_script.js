320f414f094401d7b0c29b01e3f8d94d44d95997
Fernando <100nandoo@gmail.com>
2016-11-24 17:35:34 +0700

    Skrip Angular dipersingkat
    * Controller pada skrip angular dihapus, kecuali controller main
    * tutor1.php diganti nama menjadi tutor1.html

--- js/anglr_script.js
+++ js/anglr_script.js
@@ -11,22 +11,21 @@ labApp.config(function($routeProvider) {
     // route untuk data
     .when('/data', {
       templateUrl : '../pages/data.php',
-      controller  : 'dataController'
     })
     //route untuk tambah pengunjung
     .when('/addVisitor', {
       templateUrl : '../pages/addVisitorForm.php',
-      controller  : 'addController'
     })
     // route untuk home
     .when('/home', {
       templateUrl : '../pages/home.php',
-      controller  : 'homeController'
     })
     // route untuk rtc
     .when('/tutor1', {
-      templateUrl : '../pages/tutor1.php',
-      controller  : 'tutor1Controller'
+      templateUrl : '../pages/tutor1.html',
+    })
+    .when('/tutor2', {
+      templateUrl : '../pages/tutor2.html',
     });
 });
 
@@ -34,20 +33,4 @@ labApp.config(function($routeProvider) {
 labApp.controller('mainController', function($scope) {
   // create a message to display in our view
   $scope.message = 'Everyone come and see how good I look!';
-});
-labApp.controller('dataController', function($scope) {
-  // create a message to display in our view
-  $scope.message = 'This is data!';
-});
-labApp.controller('homeController', function($scope) {
-  // create a message to display in our view
-  $scope.message = 'This is home!';
-});
-labApp.controller('addController', function($scope) {
-  // create a message to display in our view
-  $scope.message = 'This is user!';
-});
-labApp.controller('tutor1Controller', function($scope) {
-  // create a message to display in our view
-  $scope.message = 'This is tutor1!';
 });
