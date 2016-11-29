e52a2eb5778c9d875420d51c52e20265d98d6f39
Fernando <100nandoo@gmail.com>
2016-11-29 17:55:08 +0700

    backup database

--- Misc/weblogin.sql
+++ Misc/weblogin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2016 at 09:20 AM
+-- Generation Time: Nov 29, 2016 at 11:52 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -28,22 +28,39 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `buku_tamu` (
   `ke` int(10) NOT NULL,
-  `uid` varchar(20) NOT NULL,
+  `uid` varchar(20) CHARACTER SET latin1 NOT NULL,
   `masuk` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `keluar` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `buku_tamu`
 --
 
 INSERT INTO `buku_tamu` (`ke`, `uid`, `masuk`, `keluar`) VALUES
-(1, '10628203226', '2016-11-23 08:11:08', '2016-11-23 08:11:26'),
-(2, '250190214226', '2016-11-23 08:11:44', '2016-11-23 08:13:15'),
-(3, '250190214226', '2016-11-23 08:13:23', '2016-11-23 08:13:30'),
-(4, '10628203226', '2016-11-23 08:13:57', '2016-11-23 08:14:05'),
-(5, '887719016', '2016-11-23 08:18:22', '2016-11-23 08:18:41'),
-(6, '250190214226', '2016-11-23 08:18:27', '2016-11-23 08:18:51');
+(81, '10314623852', '2016-11-29 08:54:53', '2016-11-29 08:56:23'),
+(82, '250190214226', '2016-11-29 08:55:54', '2016-11-29 08:56:32'),
+(83, '221241714', '2016-11-29 08:56:03', '2016-11-29 08:56:39'),
+(84, '250190214226', '2016-11-29 09:14:17', '2016-11-29 09:18:50'),
+(85, '10314623852', '2016-11-29 09:14:27', '2016-11-29 09:19:01'),
+(86, '250190214226', '2016-11-29 10:38:46', '2016-11-29 10:39:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jumlah_pengunjung`
+--
+
+CREATE TABLE `jumlah_pengunjung` (
+  `jumlah` int(5) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `jumlah_pengunjung`
+--
+
+INSERT INTO `jumlah_pengunjung` (`jumlah`) VALUES
+(0);
 
 -- --------------------------------------------------------
 
@@ -52,9 +69,9 @@ INSERT INTO `buku_tamu` (`ke`, `uid`, `masuk`, `keluar`) VALUES
 --
 
 CREATE TABLE `logintable` (
-  `user` varchar(20) NOT NULL,
-  `pass` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `pass` varchar(20) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `logintable`
@@ -71,19 +88,20 @@ INSERT INTO `logintable` (`user`, `pass`) VALUES
 --
 
 CREATE TABLE `pengunjung` (
-  `nama` varchar(20) NOT NULL,
-  `uid` varchar(20) NOT NULL,
+  `nama` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `uid` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `privilege` tinyint(1) DEFAULT '1',
   `status` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `pengunjung`
 --
 
-INSERT INTO `pengunjung` (`nama`, `uid`, `status`) VALUES
-('Fernando', '250190214226', 0),
-('Andre Young', '10628203226', 0),
-('Bot', '887719016', 0);
+INSERT INTO `pengunjung` (`nama`, `uid`, `privilege`, `status`) VALUES
+('Giorgy', '10314623852', 3, 0),
+('Dale Watson', '221241714', 1, 0),
+('Fernando', '250190214226', 2, 0);
 
 --
 -- Indexes for dumped tables
@@ -96,6 +114,18 @@ ALTER TABLE `buku_tamu`
   ADD PRIMARY KEY (`ke`);
 
 --
+-- Indexes for table `jumlah_pengunjung`
+--
+ALTER TABLE `jumlah_pengunjung`
+  ADD PRIMARY KEY (`jumlah`);
+
+--
+-- Indexes for table `pengunjung`
+--
+ALTER TABLE `pengunjung`
+  ADD PRIMARY KEY (`uid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -103,7 +133,7 @@ ALTER TABLE `buku_tamu`
 -- AUTO_INCREMENT for table `buku_tamu`
 --
 ALTER TABLE `buku_tamu`
-  MODIFY `ke` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ke` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
